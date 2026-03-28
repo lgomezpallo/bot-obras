60887425f6d6e0ca02595ebeef237d3e4292d450
lgomezpallo <luisgomezpallo@gmail.com>
2026-03-28 09:55:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,16 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-# Tomamos el token de las variables de Railway
 TOKEN = os.environ.get("TOKEN")
-
 if not TOKEN:
-    raise ValueError("No se encontró la variable TOKEN")
+    raise ValueError("No se encontró TOKEN")
 
-# Comando /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot funcionando correctamente ✅")
+    await update.message.reply_text("Bot funcionando ✅")
 
-# Construimos la app
 app = ApplicationBuilder().token(TOKEN).build()
-
-# Agregamos el handler
 app.add_handler(CommandHandler("start", start))
 
-# Ejecutamos el bot
 if __name__ == "__main__":
     print("Bot iniciado")
     app.run_polling()
